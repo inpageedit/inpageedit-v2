f7e24574e713ea7d4be91182c2a7c5372cb7fb9b
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2020-04-01 19:07:21 +0800

    Typo

--- plugins/demo.js
+++ plugins/demo.js
@@ -1,5 +1,5 @@
 mw.hook('InPageEdit.quickEdit').add(function(){
-  $('.editArea').prepend(
+  $('.editArea').before(
     $('<div>',{id: 'ipe-edit-toolbar'}).append(
       $('<button>',{id: 'custom-btn', text: 'Foo'}).click(function(){
         // Do sth.
