0fea1291d055b2b0fc01a769d466e938c289e102
Dianliang <dianliang233@gmail.com>
2020-04-13 18:25:01 +0800

    全面换源（i18n除外）

--- script.js
+++ script.js
@@ -21,15 +21,15 @@
   /*=version*/InPageEdit.version = '2.13.0(build_2795)';/*version=*/
 
   /** 导入模态框插件 **/
-  mw.loader.load('https://cdn.bootcss.com/ssi-modal/1.0.27/js/ssi-modal.min.js');
-  $('title').after('<link id="ssi-modal-style" rel="stylesheet" href="https://cdn.bootcss.com/ssi-modal/1.0.27/styles/ssi-modal.min.css"/>');
+  mw.loader.load('https://cdn.jsdelivr.net/npm/ssi-modal@1.0.28/ssi-modal/js/ssi-modal.min.js');
+  $('title').after('<link id="ssi-modal-style" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ssi-modal@1.0.28/ssi-modal/styles/ssi-modal.min.css"/>');
 
   /** 样式表 **/
   // 皮肤
-  $('link#ssi-modal-style').after('<link rel="stylesheet" href="https://common.wjghj.cn/css/InPageEdit-v2"/>');
+  $('link#ssi-modal-style').after('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dragon-fish/inpageedit-v2@master/src/override.css"/>');
   // Material icons
-  mw.loader.load('https://cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.min.css', 'text/css');
-
+  mw.loader.load('https://cdn.jsdelivr.net/gh/google/material-design-icons@master/iconfont/material-icons.css', 'text/css');
+  
   /*** BOT FLAG ***/
   /** 导入 i18n 组件 **/
   mw.loader.load('https://common.wjghj.cn/js/i18n-js');
