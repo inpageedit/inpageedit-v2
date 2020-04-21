27f64e5b05cc4047517958452ac0ed841f8924ff
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2020-04-22 03:07:27 +0800

    Create @query.php

---
+++ analysis/api/@query.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 查询函数
+ * @param $name string Site Name
+ * @param $url string SIte URL
+ * @return $finalResult array
+ */
+function _query($sitename, $url)
+{
+    ## 调用工具类
+    require_once('/data/wwwroot/util/mongomanager.php');
+    $mongoLib = m_mgdb::i("inpageedit");
+    $collection = 'test';
+
+    ## 变量
+    $command = [];
+    $command['find'] = $collection;
+    $finalResult = [];
+    $finalResult['status'] = true;
+
+    if ($sitename) {
+        $command['filter'] = [
+            'sitename' => $sitename
+        ];
+    } elseif ($url) {
+        $command['filter'] = [
+            'url' => $url
+        ];
+    }
+
+    $resultSite = $mongoLib->command($command, []);
+    $resultSite = $resultSite->toArray();
+
+    $finalResult['msg'] = 'All query done.';
+    $finalResult['query'] = $resultSite;
+
+    ## END
+    return $finalResult;
+}
