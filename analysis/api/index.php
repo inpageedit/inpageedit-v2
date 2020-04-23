d26a832e69225877cf1de1936663882d69a4c972
机智的小鱼君 <dragon-fish@qq.com>
2020-04-23 20:21:16 +0800

    Update Analysis Module

--- analysis/api/index.php
+++ analysis/api/index.php
@@ -5,12 +5,13 @@
  * @tech used: PHP, MongoDB
  */
 
-# ini_set('display_errors', 1);
-# error_reporting(-1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 header('content-type:application/json');
 header('Access-Control-Allow-Origin:*');
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'view';
+$type = isset($_GET['type']) ? $_GET['type'] : 'site';
 $url = isset($_GET['url']) ? $_GET['url'] : false;
 $sitename = isset($_GET['sitename']) ? $_GET['sitename'] : false;
 $username = isset($_GET['username']) ? $_GET['username'] : false;
@@ -24,10 +25,19 @@ $res = [];
 switch ($action) {
 
   ## 查询
+  case '':
   case 'view':
+    $res = [
+      'status' => true,
+      'msg' => [
+        'Hello World!'
+      ],
+      'error' => 'No action param'
+    ];
+  break;
   case 'query':
     require_once('@query.php');
-    $res = _query($url, $sitename, $username, $date, $limit, $from);
+    $res = _query($type, $url, $sitename, $username, $date, $limit, $from);
   break;
 
     case 'log':
@@ -40,22 +50,29 @@ switch ($action) {
   default:
     $res = [
       'status' => false,
-      'msg' => 'Invalid param(s).',
+      'msg' => [
+        'Invalid action: ' . $action
+      ],
       $action => ['error' => 'No such action.']
     ];
     break;
 
 }
 
+## 加点骚话
+$res['msg'][] = 'InPageEdit Analysis 3.0';
+$res['msg'][] = 'Powered by MongoDB & PHP';
+$res['msg'][] = 'Author: 机智的小鱼君';
+
 // 美化输出
-if ( isset($_GET['pretty']) ) {
-  $json = json_encode($res, JSON_PRETTY_PRINT);
+if (isset($_GET['pretty'])) {
+    $json = json_encode($res, JSON_PRETTY_PRINT);
 } else {
-  $json = json_encode($res);
+    $json = json_encode($res);
 }
 // JSONP
-if ( isset($_GET['callback']) ) {
-  echo $_GET['callback'] . '(' . $json . ')';
+if (isset($_GET['callback'])) {
+    echo $_GET['callback'] . '(' . $json . ')';
 } else {
-  echo $json;
+    echo $json;
 }
