8187a2c0e193618ee4da34ee79ba741eca08d572
机智的小鱼君 <dragon-fish@qq.com>
2020-07-18 22:40:46 +0800

    Update scripts

--- analysis/mongodb_manual/update_wiki.js
+++ analysis/mongodb_manual/update_wiki.js
@@ -1,22 +1,30 @@
 /** 
- * 更新 InPageEdit Analysis wiki 数据
+ * InPageEdit Analysis 更新wiki数据
+ * @author 机智的小鱼君
+ * @description 用于更新 wiki 的站名或者域名的 MongoDB 脚本
  */
 
-// 设定查找的 wiki 以及集合
+// 查找的 wiki
 var siteID = 'https://bar.com';
-var col = 'analysis_test';
 
 // 新数据，不更新留空
 var newUrl = '';
 var newSiteName = '';
 
+// 查找的集合(一般不修改)
+var col = 'analysis_test';
+
 // 查找 wiki
-db.getCollection(col).find({ _id: siteID }).forEach(doc => {
+db.getCollection(col).find({ _id: siteID }).limit(1).forEach(doc => {
   // 是否重新生成文档
   var resetDoc = false;
 
   // 设定新的数据
   if (newSiteName !== '' && newSiteName !== undefined) {
+    // 存档旧数据
+    doc.sitename_history = doc.sitename_history || [];
+    doc.sitename_history.push(doc.sitename);
+    // 新数据
     doc.sitename = newSiteName;
   }
   if (newUrl !== '' && newUrl !== undefined) {
