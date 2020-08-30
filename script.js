fbb953180a275591d97360262b3be229358cbbaf
机智的小鱼君 <dragon-fish@qq.com>
2020-08-30 23:20:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,13 @@
  * @license GPL-3.0 GNU GENERAL PUBLIC LICENSE 3.0
  */
 !(function () {
-  try {
-    null.split();
-  } catch (error) {
-    var fileName = error.fileName;
-    var main = fileName.split('/');
-    main.pop();
-    main = main.join('/') + '/dist/InPageEdit.min.js';
-    // Load main
-    mw.loader.load(main);
-    // Send notify
-    console.warn('[InPageEdit] InPageEdit entry file has been modified. Please modify your calling URL after seeing this message.\n%c' + fileName + ' %c→ ' + main, 'text-decoration: line-through');
-  }
+  // Get script URL
+  var fileName = document.currentScript.src;
+  var main = fileName.split('/');
+  main.pop();
+  main = main.join('/') + '/dist/InPageEdit.min.js';
+  // Load main
+  mw.loader.load(main);
+  // Send notify
+  console.warn('[InPageEdit] InPageEdit entry file has been modified. Please modify your calling URL after seeing this message.\n%c' + fileName + ' %c→ ' + main, 'text-decoration: line-through');
 })();
