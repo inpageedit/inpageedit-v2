f5c2e0c92f3cde2ccdb586e696225c8a8640d547
Dragon-Fish <824399619@qq.com>
2021-07-19 23:27:38 +0800

    chore: improve code structure

--- module/test.js
+++
@@ -1,3 +0,0 @@
-module.exports = function test() {
-  console.log('InPageEdit is runing version', window.InPageEdit.version);
-}
---
+++ src/module/about.js
@@ -0,0 +1,21 @@
+const { _msg } = require('./_msg');
+
+const api = require('./api.json');
+
+/**
+ * @module about 关于插件模块
+ * @description Show "What is" modal box of IPE2
+ */
+var about = function () {
+  ssi_modal.show({
+    title: _msg('preference-about-label'),
+    className: 'in-page-edit in-page-edit-about',
+    content: $('<section>').append(
+      $('<iframe>', { style: 'margin: 0;padding: 0;width: 100%;height: 80vh;border: 0;', src: api.aboutUrl })
+    )
+  });
+}
+
+module.exports = {
+  about
+}
