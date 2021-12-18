6421a969287311529e96f71dfac018c5469194a8
机智的小鱼君 <dragon-fish@qq.com>
2021-12-18 22:55:04 +0800

    style: prettier

--- src/method/_dir.js
+++ src/method/_dir.js
@@ -5,8 +5,8 @@
  * - 注意，如果您想自己自己托管 InPageEdit，`_dir`可以直接返回您的URL
  *
  */
- const _dir =
- document?.currentScript?.src?.split('/').slice(0, -2).join('/') ??
- 'https://cdn.jsdelivr.net/npm/mediawiki-inpageedit@latest'
+const _dir =
+  document?.currentScript?.src?.split('/').slice(0, -2).join('/') ??
+  'https://cdn.jsdelivr.net/npm/mediawiki-inpageedit@latest'
 
 module.exports = _dir
